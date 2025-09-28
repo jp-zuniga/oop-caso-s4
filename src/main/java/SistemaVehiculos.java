2086b9c324f6d4a4233596632662f40d638daaa8
AliciaEA <aliestrada@uamv.edu.ni>
2025-09-28 12:27:02 -0600

    Agregue la clase motocicleta

--- src/main/java/SistemaVehiculos.java
+++ src/main/java/SistemaVehiculos.java
@@ -1,16 +1,31 @@
+import models.Automovil;
+import models.Bicicleta;
+import models.Combustible;
+import models.Vehiculo;
+
 public class SistemaVehiculos {
     public static void main(String[] args) {
         // Crear instancias de vehículos
         Vehiculo auto = new Automovil("Toyota", "Corolla", 2020, 40.0);
         Vehiculo bici = new Bicicleta("Trek", "Mountain", 2022, 21);
+        Vehiculo moto = new models.Motocicleta("Honda", "CBR500R", 2021, 15.0, 500);
 
         // Probar polimorfismo
+        System.out.println("------- Prueba de Vehículos -------");
+        System.out.println("AUTOMÓVIL:");
         auto.mover();
         System.out.println(auto.obtenerDetalles());
         ((Combustible) auto).recargarCombustible();
         System.out.println("Nivel de combustible: " + ((Combustible) auto).obtenerNivelCombustible());
 
+        System.out.println("\nBICI:");
         bici.mover();
         System.out.println(bici.obtenerDetalles());
+
+        System.out.println("\nMOTOCICLETA:");
+        moto.mover();
+        System.out.println(moto.obtenerDetalles());
+        ((Combustible) moto).recargarCombustible();
+        System.out.println("Nivel de combustible: " + ((Combustible) moto).obtenerNivelCombustible());
     }
 }
---
+++ src/main/java/models/Motocicleta.java
@@ -0,0 +1,78 @@
+package models;
+
+public class Motocicleta extends Vehiculo implements Combustible
+{
+    private double nivelCombustible;
+    private int cilindrada;
+
+    // Constructor "vacio" (tiene los atributos de Vehiculo)
+    public Motocicleta(String marca, String modelo, int anio)
+    {
+        super(marca, modelo, anio);
+    }
+
+    //Constructor de todos los atributos
+    public Motocicleta(String marca, String modelo, int anio, double nivelCombustible, int cilindrada)
+    {
+        super(marca, modelo, anio);
+        this.nivelCombustible = nivelCombustible;
+        this.cilindrada = cilindrada;
+    }
+
+    // Getters y Setters
+    public int getCilindrada()
+    {
+        return cilindrada;
+    }
+
+    public void setCilindrada(int cilindrada)
+    {
+        this.cilindrada = cilindrada;
+    }
+
+    public double getNivelCombustible()
+    {
+        return nivelCombustible;
+    }
+
+    public void setNivelCombustible(double nivelCombustible)
+    {
+        this.nivelCombustible = nivelCombustible;
+    }
+
+    // Metodos de la interfaz Combustible
+    @Override
+    public void recargarCombustible()
+    {
+        nivelCombustible = 40.0;
+        System.out.println("La motocicleta " + marca + " " + modelo + " ha sido recargada con combustible.");
+
+    }
+
+    @Override
+    public double obtenerNivelCombustible()
+    {
+        return nivelCombustible;
+    }
+
+
+    //Metodos de la clase Vehiculo
+    @Override
+    public String obtenerDetalles()
+    {
+        return super.obtenerDetalles() + ", Combustible: " + nivelCombustible + " litros, Cilindrada: " + cilindrada + "cc";
+    }
+
+    @Override
+    public void mover()
+    {
+        if (nivelCombustible > 0) {
+            System.out.println("La motocicleta " + marca + " " + modelo + " está acelerando con " + cilindrada + "cc.");
+            nivelCombustible -= 0.3;
+        } else {
+            System.out.println("La motocicleta " + marca + " " + modelo + " no tiene combustible.");
+        }
+
+
+    }
+}
